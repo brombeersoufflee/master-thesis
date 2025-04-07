93c97088b6cbf6204011b8fcd8fd189722735056
brombeersouff <tabea@rheusel.de>
2025-04-07 18:54:35 +0200

    updated main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ data_loader = data_load.DataLoader()
 image_data, np_array_data = data_loader.retina()
 
 print(len(image_data))
+print(len(np_array_data))
 
 print(image_data[0])
 print(np_array_data[0])
