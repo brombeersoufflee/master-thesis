da3a08088a91fca5e60cf6db3f57bfe428c25192
brombeersoufflee <tabea@rheusel.de>
2025-05-01 15:42:42 +0200

    model implementation begun

--- model_implementation.py
+++ model_implementation.py
@@ -1,24 +1,21 @@
 import data_load
-from tensorflow.keras import layers, models, Input
 from sklearn.model_selection import GroupKFold
+from tensorflow.keras import layers, models, Input
 from tensorflow.keras.optimizers import Nadam
+from tensorflow.keras.metrics import AUC
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 class Model_Implementation:
-    def __init__(self, model_name, kfolds=10):
+    def __init__(self, model_name, kfolds=10, augmentation=False):
         self.model_name = model_name
         self.kfolds = kfolds
+        self.augmentation = augmentation
 
         # Data loading
         self.data_loader = data_load.DataLoader()
         self.np_arrays, self.labels_data, self.patient_id, self.eye_side = self.data_loader.retina_npy()
-
-    
-    def preprocess_data(self):
-        if self.model_name == "CNN":
-            gkf = GroupKFold(n_splits=self.kfolds, random_state=53)
-            cv_split = gkf.split(X=self.np_arrays, y=self.labels_data, groups = self.patient_id)
-
-    
+        gkf = GroupKFold(n_splits=self.kfolds, random_state=53)
+        self.cv_split = gkf.split(X=self.np_arrays, y=self.labels_data, groups = self.patient_id)
 
     def build_model(self):
         if self.model_name == "CNN":
@@ -57,8 +54,39 @@ class Model_Implementation:
     def train_model(self):
         if self.model_name == "CNN":
             model = self.build_model()
-            model.compile(optimizer=Nadam(learning_rate=1e-4),  loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+            model.compile(optimizer=Nadam(learning_rate=1e-4),  loss='categorical_crossentropy', metrics=[AUC(name='auc'),'accuracy'])
             # Add your training code here
             # model.fit(...)
+            # TODO : check callback parameters
+            callbacks = [
+                EarlyStopping(monitor='val_auc', mode='max', patience=10, restore_best_weights=True),
+                ModelCheckpoint('best_model.h5', monitor='val_auc', mode='max', save_best_only=True)
+            ]
+
+            for i, (train_idx, val_idx) in enumerate(self.cv_split):
+                print(f"Fold {i+1}/{self.kfolds}")
+                X_train, X_val = self.np_arrays[train_idx], self.np_arrays[val_idx]
+                y_train, y_val = self.labels_data[train_idx], self.labels_data[val_idx]
+
+                # Data augmentation
+                if self.augmentation:
+                    raise NotImplementedError("Data augmentation is not implemented yet.")
+                else:
+                    pass
+
+                # Train the model
+                history = model.fit(
+                    X_train,
+                    validation_data=y_train,
+                    epochs=100,
+                    callbacks=callbacks,
+                    verbose=1
+                )
+
+
+                # Predict on test data
+                y_pred = model.predict(X_val)
+                auc_test = roc_auc_score(y_val, y_pred)
+                print(f"Test AUC: {auc_test:.4f}")
         else:
             raise NotImplementedError(f"Model {self.model_name} is not implemented yet.")
